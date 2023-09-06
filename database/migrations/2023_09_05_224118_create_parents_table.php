52b1b42d1c8c73ea341edfc66c6d176b2c9b89bd
Muhammad Ilyas <danish.ilyas786@gmail.com>
2023-09-07 00:44:09 +0500

    add routes

--- database/migrations/2023_09_05_224118_create_parents_table.php
+++ database/migrations/2023_09_05_224118_create_parents_table.php
@@ -16,14 +16,15 @@ return new class extends Migration
         Schema::create('parents', function (Blueprint $table) {
             $table->increments('id');
             $table->bigInteger('user_id');
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->boolean('status');
-            $table->string('phone');
-            $table->text('address');
-            $table->text('address2');
-            $table->string('phone_alternate');
-            $table->text('referral_source');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('secondary_email')->nullable();
+            $table->boolean('status')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->text('address2')->nullable();
+            $table->string('phone_alternate')->nullable();
+            $table->text('referral_source')->nullable();
             $table->bigInteger('added_by');
             $table->timestamp('added_on');
             $table->boolean('referral_from_positive_experience_with_tutor');
