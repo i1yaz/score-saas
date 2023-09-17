99e79916e6de7146d8f9da97c9cfaa1bdb9bacc4
Muhammad Ilyas <danish.ilyas786@gmail.com>
2023-09-18 03:42:08 +0500

    vite build

--- resources/views/auth/admin/login.blade.php
+++ resources/views/auth/admin/login.blade.php
@@ -58,9 +58,11 @@
                 <p class="mb-1">
                     <a href="{{ route('password.request') }}">I forgot my password</a>
                 </p>
+                @if (Route::has('register'))
                 <p class="mb-0">
                     <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
                 </p>
+                @endif
             </div>
             <!-- /.login-card-body -->
         </div>
