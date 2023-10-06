113000870d318e8b8d47b8eaf900934f5ecd9872
Muhammad Ilyas <danish.ilyas786@gmail.com>
2023-10-07 01:56:03 +0500

    add partial completion code columns

---
+++ database/migrations/2023_10_07_014049_add_columns_for_partial_completion_in_sessions_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->time('attended_duration')->nullable()->after('session_completion_code');
+            $table->boolean('charge_for_missed_session')->default(false)->after('attended_duration');
+            $table->time('charged_missed_session')->nullable()->after('charge_for_missed_session');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->dropColumn('attended_duration');
+            $table->dropColumn('charge_for_missed_session');
+            $table->dropColumn('charged_missed_session');
+        });
+    }
+};
