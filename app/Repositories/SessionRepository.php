2d2e530f706374ca5cd6b9f64d4ace3e60f2296e
Muhammad Ilyas <danish.ilyas786@gmail.com>
2023-10-15 20:48:34 +0500

    fix fullcalender events

--- app/Repositories/SessionRepository.php
+++ app/Repositories/SessionRepository.php
@@ -38,9 +38,16 @@ class SessionRepository extends BaseRepository
     {
         $start = $request->start;
         $end = $request->end;
-        $sessions = Session::select(['sessions.scheduled_date as start', 'sessions.scheduled_date as end', 'sessions.id as id', 'sessions.start_time', 'sessions.end_time', 'sessions.scheduled_date'])
+        $sessions = Session::select([
+            'sessions.scheduled_date as start',
+            'sessions.scheduled_date as end',
+            'sessions.id as id',
+            'sessions.start_time',
+            'sessions.end_time',
+            'sessions.scheduled_date'
+        ])
             ->selectRaw("CONCAT(s1.first_name,' ',s1.last_name) as title")
-            ->selectRaw("CONCAT(s2.first_name,' ',s2.last_name) as title")
+            ->selectRaw("CONCAT(s2.first_name,' ',s2.last_name) as title_s2")
             ->leftJoin('student_tutoring_packages', 'student_tutoring_packages.id', '=', 'sessions.student_tutoring_package_id')
             ->leftJoin('monthly_invoice_packages', 'monthly_invoice_packages.id', '=', 'sessions.monthly_invoice_package_id')
             ->leftJoin('students as s1', 's1.id', '=', 'student_tutoring_packages.student_id')
@@ -63,7 +70,7 @@ class SessionRepository extends BaseRepository
                 $tickMark = '✓';
             }
             $start_time = date('H:i', strtotime($session->start_time ?? ''));
-            $title = $session->title;
+            $title = $session->title??$session->title_s2;
             $session['color'] = getHexColors($i);
             $session['allDay'] = true;
             $session['title'] = "{$start_time} {$tickMark} {$title} ";
