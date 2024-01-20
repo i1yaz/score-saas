912b819c84a588deec51b16361ec597d00f2caec
i1yaz <danish.ilyas786@gmail.com>
2024-01-20 20:38:29 +0500

    git add dashboards

---
+++ app/Http/Controllers/Dashboards/ParentDashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Dashboards;
+
+use App\Http\Controllers\Controller;
+
+class ParentDashboardController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Dashboards/StudentDashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Dashboards;
+
+use App\Http\Controllers\Controller;
+
+class StudentDashboardController extends Controller{
+    public function index()
+    {
+        //
+    }
+}
