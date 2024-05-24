aabf7ee5f709c9d225b92b78c56578c0ff5de0c9
Muhammad Ilyas <danish.ilyas786@gmail.com>
2024-05-24 08:51:12 +0500

    fix

--- app/Http/Requests/UpdateTutorRequest.php
+++ app/Http/Requests/UpdateTutorRequest.php
@@ -28,7 +28,7 @@ class UpdateTutorRequest extends FormRequest
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('tutors', 'email')->ignore($this->email, 'email')],
             'picture' => ['sometimes', 'mimes:jpg,bmp,png,jpeg,JPG,BMP,PNG,JPEG', 'max:2048'],
             'resume' => ['sometimes', 'mimes:doc,docx,docm,pdf', 'max:2048'],
-            'hourly_rate' => ['required', 'numeric', 'gt:0'],
+            'hourly_rate' => ['required', 'numeric', 'gte:0'],
         ];
     }
 }
