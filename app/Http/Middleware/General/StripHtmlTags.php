c5db0428c5400f504194276d0f11e3f8ae1b7d75
Muhammad Ilyas <danish.ilyas786@gmail.com>
2024-07-14 20:42:30 +0500

    WIP

--- app/Http/Middleware/General/StripHtmlTags.php
+++ app/Http/Middleware/General/StripHtmlTags.php
@@ -16,6 +16,22 @@ class StripHtmlTags {
      * @return mixed
      */
     public function handle(Request $request, Closure $next) {
+        if (config('app.env') == 'staging') {
+            $AUTH_USER = 'tutorcentrum';
+            $AUTH_PASS = 'TutorcenTrum@123';
+            header('Cache-Control: no-cache, must-revalidate, max-age=0');
+            $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+            $is_not_authenticated = (
+                !$has_supplied_credentials ||
+                $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+                $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+            );
+            if ($is_not_authenticated) {
+                header('HTTP/1.1 401 Authorization Required');
+                header('WWW-Authenticate: Basic realm="Access denied"');
+                exit;
+            }
+        }
 
         //skip for specified 'named routes';
         if (in_array($request->route()->getName(), $this->excludedRoutes())) {
